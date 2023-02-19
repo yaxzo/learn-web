0527c36d5f00a9d380b7344dbb597a3d7c3de411
yaxzo <100714739+yaxzo@users.noreply.github.com>
2023-02-19 18:43:28 +0300

    Create main.py

---
+++ 2nd-day/main.py
@@ -0,0 +1,39 @@
+from flask import Flask
+
+from flask import render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    title = "web"
+    lot = [("Lada", "крутая тачка", "10.000"), ("Mercedes", "машина премиум класса", "5.000.000"),
+           ("Renault Logan", "средняя ценовая категория", "1.000.000")]
+
+    return render_template("index.html", title=title, lot=lot)
+
+
+@app.route("/lot/<header>/<text>/<price>")
+def lot(header, text, price):
+    title = "lot"
+    lot = [(header, text, price)]
+
+    return render_template("index.html", title=title, lot=lot)
+
+
+@app.route("/base")
+def base_index():
+    return render_template("base_index.html")
+
+
+@app.route("/extend_base")
+def extend_base():
+    lot = [("Lada", "крутая тачка", "10.000"), ("Mercedes", "машина премуим классх", "5.000.000"),
+           ("Renault Logan", "средняя ценовая категория", "1.000.000")]
+    return render_template("extend_base_index.html", lst=lot)
+
+
+if __name__ == '__main__':
+    app.run(host="127.0.0.1", port=5050)
